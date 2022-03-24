35ebcf1859d4ac979edf3945872a7b4496d165b9
fransoa2103 <73296404+fransoa2103@users.noreply.github.com>
2022-03-24 17:14:36 +0100

    C:/Program Files/Git/chap 7/loop_post/themes folder/have_post

--- wp-content/themes/montheme/functions.php
+++ wp-content/themes/montheme/functions.php
@@ -3,6 +3,7 @@
 function montheme_supports()
 {
     add_theme_support('title-tag');
+    add_theme_support('post-thumbnails');
 }
 
 // register libraries
--- wp-content/themes/montheme/index.php
+++ wp-content/themes/montheme/index.php
@@ -1,3 +1,32 @@
 <?php get_header() ?>
-Bonjour TLM
+
+<?php if (have_posts()) : ?>
+
+    <div class="row">
+
+        <?php while (have_posts()) : the_post(); ?>
+            <div class="col-sm-4">
+                <div class="card" style="width: 18rem;">
+                    <?php the_post_thumbnail('medium', ['class'=>'card-img-top', 'alt'=>'', 'style'=>'height: auto']) ?>
+                    <img class="card-img-top" src="" alt="" srcset="">
+                    <div class="card-body">
+                        <h5 class="card-title"><?php the_title(); ?></h5>
+                        <h6 class="card-subtitle mb-2 text-muted"><?php the_category(); ?></h6>
+                        <p class="card-text">
+                            <?php the_content('En voir plus'); ?>
+                        </p>
+                        <a href="<?php the_permalink(); ?>" class="card-link">Voir plus</a>
+                        <a href="#" class="card-link">Another link</a>
+                    </div>
+                </div>
+            </div>
+        <?php endwhile ?>
+
+    </div>
+    <!-- /end/div/class row -->
+
+<?php else : ?>
+    <h3>pas d'articles en cours</h3>
+<?php endif ?>
+
 <?php get_footer() ?>
